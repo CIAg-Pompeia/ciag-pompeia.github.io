6a9e0e6409c0fb06ed17b686e0a901cbafec001c
Matheus Castro <matheus.castro@ciag.org.br>
2023-03-03 11:15:29 -0300

    fix: oopsie

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ fetch("https://api.github.com/repos/CIAg-Pompeia/ciag-pompeia.github.io/git/tree
     .then(e => e.json())
     .then(e => {
         console.log(e.tree.filter(t => t.type == 'tree'))
-        const projects = (e.tree.filter(t => t.type == 'tree')).map(m => {
-            console.log({ name: m.path, url: window.location.href + '/' + m.path, cover: `https://raw.githubusercontent.com/CIAg-Pompeia/ciag-pompeia.github.io/main/${m.path}/cover.png` })
-            return ({ name: m.path, url: window.location.href + '/' + m.path, cover: `https://raw.githubusercontent.com/CIAg-Pompeia/ciag-pompeia.github.io/main/${m.path}/cover.png` })
+        const projects = (e.tree.filter(t => t.type == 'tree' && t.path !== 'assets')).map(m => {
+            return ({ name: m.path, cover: `https://raw.githubusercontent.com/CIAg-Pompeia/ciag-pompeia.github.io/main/${m.path}/cover.png` })
         });
         projects.forEach(project => {
-            document.querySelector("#projects").appendChild(parseHTML(`<div>
-                <img src="${project.cover}" onerror="this.src='_assets/empty.svg'"/>
+            document.querySelector("#projects").appendChild(parseHTML(`<div onclick="window.location='${project.name}';">
+                <img src="${project.cover}" onerror="this.src='assets/empty.svg'"/>
                 <p>${project.name}</p>
             </div>`).body.firstChild)
         });
